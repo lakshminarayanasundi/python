c23db867f2657519693e6e9e7b177de10c05425a
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:18:53 +0530

    Add files via upload

---
+++ if conditions.py
@@ -0,0 +1,164 @@
+#if-condition by using comparision operators
+#<,>,<=,>=,!=,==
+'''a=10
+b=15
+if a<b:
+    print("true")'''
+
+'''a=6
+b=9
+if a>b:
+    print("true")'''
+
+'''a=20
+b=40
+if a!=b:
+    print("not equal")'''
+
+'''a=12
+b=15
+if a<=b:
+    print("true")'''
+
+'''a=10
+b=15
+if a<=b:
+    print("true")'''
+    
+'''a=5
+b=5
+if a==b:
+    print("equal")'''
+
+
+'''a=9
+if a>3:
+    print("greater")'''
+
+'''a="python"
+if a=="python":
+   print("equal")'''
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+if a<b:
+    print("less")'''
+
+'''a="python"
+if a!="python":
+    print(" equal")'''
+
+'''a=input("data")
+if a=="python":
+    print("true")'''
+
+#if-condition by using logical operators
+#and,or,not
+'''a=5
+b=7
+if a<b and b>a:
+    print("true")'''
+
+'''a=9
+b=11
+if a<=b and b>=a:
+    print("true")'''
+'''a=4
+b=6
+if a!=b and a==b:
+    print("false")'''
+
+'''a=8
+b=12
+if a!=b or b==a:
+    print("true")'''
+'''a=5
+b=9
+if not a<b:
+    print("less")'''
+'''a=5
+b=9
+if not a<b and b>a:
+    print("less")'''
+'''a=5
+b=9
+if not a<b or b>a:
+    print("true")'''
+    
+#if-condition by using identify operators
+#is,isnot
+'''a=7
+if type(a) is int:
+    print("it is not")'''
+
+'''a=9
+if type(a) is not int:
+    print("false")'''
+
+
+'''a=int(input("a value"))
+if type(a) is int:
+    print("true")'''
+    
+
+'''a=6.9
+if type(a) is float:
+    print("true")'''
+
+
+'''a="Lucky"
+if type(a) is str:
+    print("true")'''
+#if-condition  by using membership operators
+#in,not in
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("true")'''
+
+'''a=2,3,4,5,6,7,8,9,10
+if 10 not in a:
+    print("true")'''
+
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")'''#error
+
+'''a=[4,5,6,7,8,9,10,11]
+b=int(input("value"))
+if b in a:
+    print("true")'''
+    
+    
+#if-else condition by using comparision operators
+'''a=3
+b=6
+if a<b:
+    print("less")
+else:
+    print("true")'''
+
+
+'''a=3
+b=6
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+
+
+'''a=3
+b=6
+if a>b:
+    print("less")
+else:
+    a<=b
+    print("true")'''
+
+
+'''a=19
+b=17
+if a>b and b>a:
+        print("true")
+else:
+    print("false")'''
