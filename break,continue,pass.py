aef110c1f2e0368ac886ffde0607f9379ef6804c
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:21:45 +0530

    Add files via upload

---
+++ break,continue,pass.py
@@ -0,0 +1,84 @@
+#difference btw break,continueand pass
+#break
+#break is used to terminate the entire loop.
+#continue
+#continue is used to skips the current iteration and rest of the code will be continue.
+#pass
+#a pass is null statement it does nothing but syntatically we need.
+
+
+#break
+'''a=10
+while a>1:
+    print(a)
+    a=a-1
+    if a==6:
+        break'''
+
+
+'''a=20
+while a>3:
+    a=a-1
+    if a==6:
+       break
+    print(a)'''
+
+
+
+
+'''for i in range(25):
+    if i==19:
+        break
+    print(i)'''
+
+
+'''a="python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+
+#continue
+'''a=30
+while a>5:
+    print(a)
+    a=a-1
+    if a==15:
+       continue'''
+'''a=30
+while a>5:
+    a=a-1
+    if a==15:
+       continue
+    print(a)'''
+
+'''for i in range(15):
+    if i==11:
+        continue
+    print(i)'''
+
+
+'''a="python"
+for i in a:
+    if i=="h":
+        continue
+    print(i)'''
+
+#pass
+
+'''a=9
+while a>2:
+    print(a)
+    a=a-1
+    if a==7:
+        pass'''
+
+
+'''for i in range(25):
+    if i==20:
+        pass
+    print(i)'''
+
+
+
