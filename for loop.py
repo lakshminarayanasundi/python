58ab5301dc8ffeeefc12163d8f9286285bd1d3f8
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:26:12 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,39 @@
+#loops
+#for,while,range,break,continues,pass
+#for loop()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''b=(5,6,7,8,9,)
+for i in b:
+    print(i)
+print(type(b))
+print(type(i)'''
+
+'''a={3,4,5,6,7,}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))''
+      
+a={"name:""pooja","city":"vja","state:"ap",year":2026}
+for i in a:
+    print(i)
+print(a.keys())
+print(type(a))
+print(type(
