7bf674730a25e8c3945e30b3f78b43d1b778cb22
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:20:04 +0530

    Add files via upload

---
+++ if condition 3.py
@@ -0,0 +1,70 @@
+#social-media-login
+#nested if 
+'''user=input("enter the name")
+password=int(input("enter the password"))
+if user=="pooja":
+    if password==1234:
+        print("successfull")
+else:
+    print("not match")'''
+
+
+'''user=input("enter the name")
+password=int(input("enter the password"))
+if user=="pooja":
+    if password==1234:
+        print("successfull")
+    else:
+       print("not match")'''
+
+
+'''user=input("enter the name")
+password=int(input("enter the password"))
+if user=="pooja"and password=="pooja@1234":
+    print("successfull")
+else:
+    print("not match")'''
+#multiple if
+'''age=int(input("enter the age"))
+marks=int(input("enter the marks"))
+attendance=int(input("enter the attendance"))
+if age>=18:
+    print("eligible for vote")
+if marks>=70:
+     print("eligibile for exam")
+if attendance>=80:
+     print("eligibile for scholarship")'''
+
+#if-else-if
+'''cakename=input("enter the cake name")
+if cakename==("red velvet"):
+    print(1200)
+elif cakename=="choco almond":
+    print(1000)
+elif cakename=="honey almond":
+    print(800)
+elif cakename=="butter scotch":
+    print(600)
+else:
+    print("cake is not available")'''
+
+
+'''price=int(input("enter the price"))
+if price==1000:
+    print("bbq pizza")
+elif price==800:
+    print("crispy chicken pizza")
+elif price==600:
+    print("panner pizza")
+elif price==400:
+    print("corn pizza")
+elif price==200:
+    print("french fries & coke")'''
+
+
+
+
+        
+    
+    
+ 
