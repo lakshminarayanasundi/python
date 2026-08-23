adb3cd39756bca1b9af3a0d13aa2c1a9e7a0a87b
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:17:47 +0530

    Add files via upload

---
+++ runtime input options.py
@@ -0,0 +1,108 @@
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(choose the option 1.add 2.sub 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option+input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+#swapping of two numbers
+'''a=10
+b=20
+a,b=b,a
+print("a value is",a)
+print("b value is",b)'''
+
+
+
+'''a=10
+b=20
+temp=a
+a=b
+b=temp
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("a value is",a)
+print("b value is",b)'''
+
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("after swapping a=%d,b=%d" %(a,b))'''
+
+
+#a=["codegnan","python","course"]
+#["CODEGNAN","PYTHON","COURSE"]
+'''a=["codegnan","python","course"]
+b=str(a)
+print(b.upper())'''
+
+
+'''a=[10,20,30,40,50,"code"]
+#[10,20,30,40,50"c"o"d"e"]
+
+a.extend("code")
+print(a)'''
+
+'''a=[5,7,9,10,11,13]
+a.insert(5,12)
+print(a)'''
+
+'''a=[9,7,4,0,1,5,10,8,6,3]
+a.sort()
+print(a)'''
+
+'''a=("apple","banana","mango")
+b=list(a)
+b.append("grapes")
+print(b)
+
+c=tuple(b)
+print(c)
+print(type(c))'''
+
+
+
+
+
+idno=int(input("enter the idno"))
+name=input("enter the name")
+mobileno=int(input("enter the mobile no"))
+mailid=input("enter the mailid")
+branch=input("enter the branch")
+college=input("enter the collage name")
+print("............student profile...........")
+print("idno is",idno)
+print("name is",name)
+print("mobile no",mobileno)
+print("mailid is",mailid)
+print("branch is",branch)
+print("college is",college)
+           
