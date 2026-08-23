634a44c58c424bf4ed41c27a41b2e93b6eb625e3
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:30:51 +0530

    Add files via upload

---
+++ map func.py
@@ -0,0 +1,77 @@
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+
+'''a,b=input("enter the data").split(",")
+print(a+b)'''
+
+
+'''a,b=[x for x in input("enter the names").split(",")]
+print(a+b)'''
+
+
+'''a,b=map(str,input("enter the data").split(","))
+print(a+b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+print(a+b)'''
+
+
+'''a,b=[int(x) for x in input("enter the values").split(",")]
+print(a+b)'''
+
+'''a,b=[int(input("enter the values").split(",")]
+print(a+b)'''#error
+
+'''a,b=[map(input("enter the values").split(",")]
+print(a+b)'''
+
+
+'''a=list(map(int,input("values").split(",")))
+print(a)
+print(type(a))'''
+
+
+'''a=tuple(map(int,input("values").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=set(map(int,input("values").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=input("enter the key and values")
+b=dict(i.split(":") for i in a.split(","))
+print(b)'''
+
+'''a=list(map(str,input("values").split(",")))
+print(a)
+print(type(a))'''
+
+'''a=list(map(eval,input("values").split(",")))
+print(a)
+print(type(a))'''
+
+
+#difference btw module,library,package.
+#a module in python is single python file it consist python code.
+#examples of modules include math.py,random.py,mymodule.py.
+#it contains func,classes,variables.
+
+#package
+#one or more modules and an __init__.py
+#examples of packages included request,numpy,pandas.
+
+
+#library
+#it consist both modules and packages
+#ex of library such as numpy,pandas,matplotlib.
+
+#Note
+#every python file is amodule and import is keyword and every python file is saved internally with variable name as __main__
+
+
+
+
