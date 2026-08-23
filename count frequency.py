31ab1b8622d20bcdfdff401013186ac5229c7b02
lakshminarayanasundi <lakshminarayanasundi23@gmail.com>
2026-08-23 10:36:51 +0530

    Add files via upload

---
+++ count frequency.py
@@ -0,0 +1,30 @@
+'''def count_frequency(x):
+    freq = {}
+    for ch in x:
+        freq[ch] = freq.get(ch, 0) + 1
+    return ''.join(f"{ch}{count}" for ch, count in freq.items())
+
+x = "aabbcc"
+output = count_frequency(x)
+print(output)  '''
+
+
+
+'''def count_frequency(x):
+    freq = {}
+    for ch in x:
+        freq[ch] = freq.get(ch, 0) + 1
+    return ''.join(f"{ch}{count}" for ch, count in freq.items())'''
+
+'''x = "abcada"
+output = count_frequency(x)
+print(output)'''
+
+'''word = "data science"
+reversed_word = word[::-1]
+print(reversed_word)'''
+
+
+word = "data "
+reversed_word = word[::-1]
+print(reversed_word)
